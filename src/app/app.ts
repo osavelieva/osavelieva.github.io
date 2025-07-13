fbb7e7fe3623f575c357fd13e936787fddb593ff
Olena Savelieva <45556723+osavelieva@users.noreply.github.com>
2025-07-13 15:40:37 +0300

    Add skills per job

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,24 +1,39 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import * as resumeJson from '../data/resume.json';
-import { NgClass } from '@angular/common';
 import { DatePipe } from '@angular/common';
+import * as resumeJson from '../data/resume.json';
+import { ResumeSchema } from '../shared/interfaces/resume.interface';
 
 @Component({
   selector: 'app-root',
-  imports: [DatePipe, NgClass],
+  imports: [DatePipe],
   templateUrl: './app.html',
   styleUrl: './app.sass',
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: {
-    class: 'block' //max-w-screen-xl mx-auto p-4 sm:p-6 lg:p-8
+    class: 'block'
   }
 })
 export class App {
-  protected readonly resume = resumeJson;
-  protected readonly workItems = this.resume.work.map(workItem => ({
-    ...workItem,
-    summaryItems: workItem.summary.split('\n').map(item => item.replace('• ', '').replace('- ', '')).filter(Boolean)
-  }));
-  protected readonly skills = this.resume.skills.map(skill => skill.name).sort((a, b) => a.localeCompare(b));
+  protected readonly resume: ResumeSchema = resumeJson;
+  protected readonly workItems = this.summaryItemsMapper();
+  protected readonly skills = (this.resume.skills ?? [])
+    .map(skill => skill.name)
+    .sort((a, b) => (a ?? '').localeCompare(b ?? ''));
   protected readonly currentYear = new Date().getFullYear();
+
+  private summaryItemsMapper() {
+    const withSummaryLines = (this.resume.work ?? []).map(workItem => ({
+      ...workItem,
+      summaryLines: (workItem.summary ?? '').split('\n')
+    }));
+
+    return withSummaryLines.map(workItem => ({
+      ...workItem,
+      summaryItems: workItem.summaryLines
+        .slice(0, workItem.summaryLines.length - 1)
+        .map(item => item.replace('• ', ''))
+        .filter(Boolean),
+      skills: workItem.summaryLines.slice(-1)[0].split(' · ')
+    }));
+  }
 }
